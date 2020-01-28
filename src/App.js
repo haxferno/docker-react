267de3d4471ff49bbb13acdbc52716e387557ad4
Inferno24 <inferno.pranjal24@gmail.com>
2020-01-28 10:54:15 +0530

    updated the feature app files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World - Feature Branch
+          Welcome to arc - Feature Branch
         </p>
         <a
           className="App-link"
