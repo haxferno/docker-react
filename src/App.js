bfd8a8e4068f773cecbe1fefe5112e7c2bf9dd75
Inferno24 <inferno.pranjal24@gmail.com>
2020-01-28 11:37:11 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my Arc-React Project
-          Created using:
-          - Docker
-          - Node.js
-          - Git
-          - Github
-          - Travis-ci
-          - AWS Elasticbeanstalk
+          Welcome to my Arc-React Project<br />
+          <br />
+          Created using:<br />
+          - Docker<br />
+          - Node.js<br />
+          - Git<br />
+          - Github<br />
+          - Travis-ci<br />
+          - AWS Elasticbeanstalk<br />
+          <br />
+          <a href="https://github.com/Inferno24/docker-react">GitHub</a>?<br />
         </p>
         <a
           className="App-link"
-          href="https://github.com/Inferno24/docker-react"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
